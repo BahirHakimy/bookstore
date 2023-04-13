3b9ef7a0fa5f00e8b832b5205bf408e7574548ae
Bahir Hakimy <bahirhakimy2015@gmail.com>
2023-04-13 23:19:36 +0430

    Update bookSlice.js added api calls

--- src/redux/features/books/bookSlice.js
+++ src/redux/features/books/bookSlice.js
@@ -1,68 +1,58 @@
-import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
 
 const initialState = {
-  items: [
-    {
-      item_id: 'item1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-      progress: 45,
-      currentChapter: 'Chapter 7',
-      circleStyles:
-        'bg-[radial-gradient(closest-side,_white_79%,_transparent_80%_100%),conic-gradient(#379cf6_64%,_#e8e8e8_0)]',
-    },
-    {
-      item_id: 'item2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
-      progress: 33,
-      currentChapter: 'Chapter 7',
-      circleStyles:
-        'bg-[radial-gradient(closest-side,_white_79%,_transparent_80%_100%),conic-gradient(#379cf6_64%,_#e8e8e8_0)]',
-    },
-    {
-      item_id: 'item3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
-      progress: 25,
-      currentChapter: 'Chapter 7',
-      circleStyles:
-        'bg-[radial-gradient(closest-side,_white_79%,_transparent_80%_100%),conic-gradient(#379cf6_64%,_#e8e8e8_0)]',
-    },
-    {
-      item_id: 'item4',
-      title: 'The Hunger Games',
-      category: 'Action',
-      author: 'Suzanne Collins',
-      progress: 64,
-      currentChapter: 'Chapter 17',
-      circleStyles:
-        'bg-[radial-gradient(closest-side,_white_79%,_transparent_80%_100%),conic-gradient(#379cf6_64%,_#e8e8e8_0)]',
-    },
-    {
-      item_id: 'item5',
-      title: 'Dune',
-      category: 'Science Fiction',
-      author: 'Frank Herbert',
-      progress: 8,
-      currentChapter: 'Chapter 3: "A Lesson Learned"',
-      circleStyles:
-        'bg-[radial-gradient(closest-side,_white_79%,_transparent_80%_100%),conic-gradient(#379cf6_8%,_#e8e8e8_0)]',
-    },
-    {
-      item_id: 'item6',
-      title: 'Capital in the Twenty-First Century',
-      category: 'Economy',
-      author: 'Suzanne Collins',
-      progress: 0,
-      currentChapter: 'Introduction',
-      circleStyles:
-        'bg-[radial-gradient(closest-side,_white_79%,_transparent_80%_100%),conic-gradient(#379cf6_0%,_#e8e8e8_0)]',
-    },
-  ],
+  items: [],
+  loading: [],
+};
+
+export const fetchBooks = createAsyncThunk('books/fetchBooks', async () => {
+  const response = await axios.get(
+    `${process.env.REACT_APP_API_URL}/apps/${process.env.REACT_APP_API_ID}/books`
+  );
+  return response.data;
+});
+
+export const createBook = createAsyncThunk(
+  'books/createBook',
+  async ({ title, author }, { getState }) => {
+    try {
+      const book = {
+        item_id: `item${getState().books?.items.length}`,
+        category: 'Action',
+        title,
+        author,
+      };
+      axios.post(
+        `${process.env.REACT_APP_API_URL}/apps/${process.env.REACT_APP_API_ID}/books`,
+        book
+      );
+      return book;
+    } catch (error) {
+      return error;
+    }
+  }
+);
+export const deleteBook = createAsyncThunk(
+  'books/deleteBook',
+  async ({ item_id }) => {
+    await axios.delete(
+      `${process.env.REACT_APP_API_URL}/apps/${process.env.REACT_APP_API_ID}/books/${item_id}`
+    );
+    return item_id;
+  }
+);
+
+const createItem = ([item_id, data]) => {
+  const progress = Math.round(Math.random() * 100);
+  return {
+    item_id,
+    ...data[0],
+    progress,
+    currentChapter: 'Introduction',
+    circleStyles: `bg-[radial-gradient(closest-side,_white_79%,_transparent_80%_100%),conic-gradient(#379cf6_67%,_#e8e8e8_0)]`,
+  };
 };
 
 const bookSlice = createSlice({
@@ -88,6 +78,59 @@ const bookSlice = createSlice({
       ...state,
       items: state.items.filter((item) => item.category === payload.category),
     }),
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchBooks.pending, (state) => {
+      const id = toast.loading('Loading Please wait...');
+      return { ...state, loading: [...state.loading, id] };
+    });
+
+    builder.addCase(fetchBooks.fulfilled, (state, action) => {
+      const updatedLoading = [...state.loading]; // Create a copy of the loading array
+      toast.dismiss(updatedLoading.pop());
+      return {
+        ...state,
+        items: Object.entries(action.payload).map(createItem),
+        loading: updatedLoading, // Update the loading array without the last element
+        error: null,
+      };
+    });
+
+    builder.addCase(fetchBooks.rejected, (state) => {
+      const updatedLoading = [...state.loading]; // Create a copy of the loading array
+      toast.dismiss(updatedLoading.pop());
+      return { ...state, loading: [] };
+    });
+
+    builder.addCase(createBook.pending, (state) => {
+      const id = toast.loading('Posting Please wait...');
+      return { ...state, loading: [...state.loading, id] };
+    });
+
+    builder.addCase(createBook.fulfilled, (state, { payload }) => {
+      const updatedLoading = [...state.loading]; // Create a copy of the loading array
+      toast.dismiss(updatedLoading.pop());
+
+      state.loading = updatedLoading;
+      state.items.push({
+        ...payload,
+        progress: Math.round(Math.random() * 100),
+        currentChapter: 'Introduction',
+        circleStyles: `bg-[radial-gradient(closest-side,_white_79%,_transparent_80%_100%),conic-gradient(#379cf6_67%,_#e8e8e8_0)]`,
+      });
+    });
+
+    builder.addCase(deleteBook.pending, (state) => {
+      const id = toast.loading('Deleting Please wait...');
+      return { ...state, loading: [...state.loading, id] };
+    });
+
+    builder.addCase(deleteBook.fulfilled, (state, { payload }) => {
+      const updatedLoading = [...state.loading]; // Create a copy of the loading array
+      toast.dismiss(updatedLoading.pop());
+      state.loading = updatedLoading;
+      state.items = state.items.filter((item) => item.item_id !== payload);
+    });
   },
 });
 
